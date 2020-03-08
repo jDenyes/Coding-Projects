f487645081c5e2385e2dc2b72f0ff79a586e4ec5
Jacob Denyes <15jd56@caslab.queensu.ca>
2020-03-07 20:08:39 -0500

    code clean up

--- StockDiversification/app.js
+++ StockDiversification/app.js
@@ -17,17 +17,10 @@ app.get('/', (req, res) => {
     res.end();
 });
 
-// app.get('/TickerResponse', (req,res) => {
-//     res.sendFile((path.join(__dirname + '/index.html'));
-// });
-
 function callback(data, res) {
     console.log("retrieved data successfully");
     console.log(data);
 
-    // now sending the data back to the html page
-
-    // console.log(data);
     res.send(data);
     res.end();
 }
@@ -40,17 +33,7 @@ app.post('/', (req, res) => {
 
     console.log(quantity + " " + ticker);
     let url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=".concat(ticker.concat('&outputsize=full&apikey=7KZAUZJR7I5MDRU0'));
-    // url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&outputsize=full&apikey=demo";
-    // res.end();
     AAPI.GetCurrentStockPrice(url, callback, res);
-
-    // res.sendFile(path.join(__dirname + '/index.html'));
-    // res.end();
 });
-
-// app.get('/posted', (req, res) => {
-//     res.sendFile(path.join(__dirname + '/index.html'));
-//     res.end();
-// });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
