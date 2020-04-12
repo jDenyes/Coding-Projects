06a063626a1b0b2728f78372b1e0039e2f35bbd4
Jacob Denyes <jacob.denyes@gmail.com>
2020-04-12 16:05:41 -0400

    idk what this is

--- Node Routing/app.js
+++ Node Routing/app.js
@@ -4,13 +4,13 @@ var express = require("express");
 var app = express();
 app.set('view engine', 'ejs');
 
-app.listen(3000);
+app.listen(5000);
 
 app.get('/', function(req, res) {
     res.render('about');
 });
 
-app.get('*', function(req, res) {
-    res.render('error');
-});
+// app.get('*', function(req, res) {
+//     res.render('error');
+// });
 
