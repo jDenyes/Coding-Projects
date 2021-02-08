63dd3b5a5863e366218e0aa1ac82ef4924fcb015
jDenyes <jacob.denyes@gmail.com>
2021-02-08 00:30:33 -0500

    made changes to python file

--- StockDiversification/ShowDiversification.py
+++ StockDiversification/ShowDiversification.py
@@ -1,8 +1,20 @@
-import matplotlib.pyplot as plt
+# To be run from teh desktop
 
-fig = plt.figure()
-ax = fig.add_axes([0,0,1,1])
-stocks = ['MSFT', 'EMB', 'AMD']
-prices = [1, 4, 2]
-ax.bar(stocks, prices)
-plt.show()
+# import matplotlib.pyplot as plt
+import usrllib
+from smb.SMBHandler import SMBHandler
+
+opener = urllib.request.build_opener(SMBHandler)
+fh = opener.open('smb://192.168.0.05/homepi/Coding/Coding-Project/StockDiversification');
+data = fh.read()
+fh.close()
+
+print(data)
+
+
+# fig = plt.figure()
+# ax = fig.add_axes([0,0,1,1])
+# stocks = ['MSFT', 'EMB', 'AMD']
+# prices = [1, 4, 2]
+# ax.bar(stocks, prices)
+# plt.show()
